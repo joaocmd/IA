5db3200074eced9cbb60235eec637aa9a61a937a
pgalhardo <pedromgalhardo@tecnico.ulisboa.pt>
2019-10-03 19:13:20 +0100

    Solve ex1

--- ruagomesfreiregame1.py
+++ ruagomesfreiregame1.py
@@ -83,54 +83,54 @@ if validatepath(nn,I,U, tickets = [5,5,2]):
 else:
         print("invalid path")
 
-print("\n(6 val) Exercise 3 - Three agents, No limits (test 1)")
-print("Init [1,3,7] Goal [2,21,9]")
-SP = SearchProblem(goal = [2,21,9], model = U, auxheur=coords)
-tinit = time.process_time()
-I = [1,3,7]
-nn = SP.search(I,limitexp = 2000)
-tend = time.process_time()
-print("%.1fms"%((tend-tinit)*1000))
-if validatepath(nn,I,U):
-        print("path")
-        print(nn)
-        plotpath(nn,coords)
-else:
-        print("invalid path")
+# print("\n(6 val) Exercise 3 - Three agents, No limits (test 1)")
+# print("Init [1,3,7] Goal [2,21,9]")
+# SP = SearchProblem(goal = [2,21,9], model = U, auxheur=coords)
+# tinit = time.process_time()
+# I = [1,3,7]
+# nn = SP.search(I,limitexp = 2000)
+# tend = time.process_time()
+# print("%.1fms"%((tend-tinit)*1000))
+# if validatepath(nn,I,U):
+#         print("path")
+#         print(nn)
+#         plotpath(nn,coords)
+# else:
+#         print("invalid path")
 
-print("\n(6 val) Exercise 3 - Three agents, No limits (test 2)")
-print("Init [30,40,109] Goal [61,60,71]")
-SP = SearchProblem(goal = [61,60,71], model = U, auxheur=coords)
-tinit = time.process_time()
-I = [30,40,109]
-nn = SP.search(I,limitexp = 2000)
-tend = time.process_time()
-print("%.1fms"%((tend-tinit)*1000))
-if validatepath(nn,I,U):
-        print("path")
-        print(nn)
-        plotpath(nn,coords)
-else:
-        print("invalid path")
+# print("\n(6 val) Exercise 3 - Three agents, No limits (test 2)")
+# print("Init [30,40,109] Goal [61,60,71]")
+# SP = SearchProblem(goal = [61,60,71], model = U, auxheur=coords)
+# tinit = time.process_time()
+# I = [30,40,109]
+# nn = SP.search(I,limitexp = 2000)
+# tend = time.process_time()
+# print("%.1fms"%((tend-tinit)*1000))
+# if validatepath(nn,I,U):
+#         print("path")
+#         print(nn)
+#         plotpath(nn,coords)
+# else:
+#         print("invalid path")
         
-print("\n(4 val) Exercise 4 - Three agents, Limits")
-print("Init [30,40,109] Goal [61,60,71]")
-SP = SearchProblem(goal = [63,61,70], model = U, auxheur=coords)
-tinit = time.process_time()
-I = [30,40,109]
-nn = SP.search(I,limitexp = 3000, limitdepth = 10, tickets = [5,20,2])
-tend = time.process_time()
-print("%.1fms"%((tend-tinit)*1000))
-if validatepath(nn,I,U, tickets = [5,20,2]):
-        print("path")
-        print(nn)
-        plotpath(nn,coords)
-else:
-        print("invalid path")
+# print("\n(4 val) Exercise 4 - Three agents, Limits")
+# print("Init [30,40,109] Goal [61,60,71]")
+# SP = SearchProblem(goal = [63,61,70], model = U, auxheur=coords)
+# tinit = time.process_time()
+# I = [30,40,109]
+# nn = SP.search(I,limitexp = 3000, limitdepth = 10, tickets = [5,20,2])
+# tend = time.process_time()
+# print("%.1fms"%((tend-tinit)*1000))
+# if validatepath(nn,I,U, tickets = [5,20,2]):
+#         print("path")
+#         print(nn)
+#         plotpath(nn,coords)
+# else:
+#         print("invalid path")
 
-print("\n(4 val) Exercise 5 - Three agents, Limits, Any-Order")
+# print("\n(4 val) Exercise 5 - Three agents, Limits, Any-Order")
 
-tendtotal = time.process_time()
-print("Total time %.1fms"%((tendtotal-tinittotal)*1000))
+# tendtotal = time.process_time()
+# print("Total time %.1fms"%((tendtotal-tinittotal)*1000))
 
 
--- ruagomesfreiregamesol.py
+++ ruagomesfreiregamesol.py
@@ -1,20 +1,76 @@
 import math
 import pickle
 import time
+from enum import Enum
 
-  
+TAXI = 0
+AUTOCARRO = 1
+METRO = 2
+
+TRANSPORT = 0
+DEST = 1
+
+class BFSNode:
+  def __init__(self, id, origin_node, transport):
+    self.id = id
+    self.origin_node = origin_node
+    self.transport = transport
+
+  def __repr__(self):
+    return f"({self.transport}, {self.id})"
+
+
 class SearchProblem:
 
   def __init__(self, goal, model, auxheur = []):
-    ##
-    ## to implement
-    ## 
-    pass
+    self.map = model
+    self.goal = goal
 
-  def search(self, init, limitexp = 2000, limitdepth = 10, tickets = [math.inf,math.inf,math.inf]):
-    ##
-    ## to implement
-    ##
+  def traceback(self, dest):
+    current_node = dest
+    solution =  []
+    while current_node != []:
+      transports = [] if current_node.transport == None else [current_node.transport]
+      solution.insert(0, [transports, [current_node.id]])
+      current_node = current_node.origin_node
+    
+    return solution
+      
+  def in_frontier(self, frontier, id):
+    for node in frontier:
+      if node.id == id:
+        return True
+
+    return False
+
+  def BFS(self, init, goal):
+
+    nodes = [None] * 114     # node list
+    nodes[init[0]] = BFSNode(init[0], [], None) 
+
+    explored = [False] * 114 # bool list
+    frontier = [nodes[init[0]]]     # node list
+
+    while frontier != []:
+      node = frontier.pop()
+      explored[node.id] = True
+      # print(f"-----------------EXPANDING-----------------")
+      # print(f"From: {node.id}")
+      # print(self.map[node.id])
+      
+      for path in self.map[node.id]:
+        dest = path[DEST]
+        #print(f"To: ({path[TRANSPORT]}, {dest})")
+        if not explored[dest] and not self.in_frontier(frontier, dest): #O(n)
+          nodes[dest] = BFSNode(dest, node, path[TRANSPORT])
+          # print("comparing ", dest, goal)
+          if dest == goal[0]: # reached goal, traceback
+            return self.traceback(nodes[dest])
+            
+          frontier.append(nodes[dest])
+
     return []
 
-    
+
+  def search(self, init, limitexp = 2000, limitdepth = 10, tickets = [math.inf,math.inf,math.inf]):
+    return self.BFS(init, self.goal)
